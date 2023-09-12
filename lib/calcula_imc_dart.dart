481a0235d59e3e1806aacbc645ff4f78e9bb1363
Priscila Silva <priscilacds_100@yahoo.com.br>
2023-09-12 19:59:11 -0300

    Correção classe pessoa

--- lib/calcula_imc_dart.dart
+++ lib/calcula_imc_dart.dart
@@ -1,15 +1,16 @@
 import 'package:calcula_imc_dart/models/ConsoleUtils.dart';
+import 'package:calcula_imc_dart/models/Pessoa.dart';
 
 void execute() {
+  Pessoa pessoa = Pessoa();
 
   print('Calcule o seu IMC!');
-  String nome = ConsoleUtils.lerStringTexto('Informe o seu nome:');
+  
+  String nome = pessoa.nome = ConsoleUtils.lerStringTexto('Informe o seu nome:');
   print('Olá $nome!');
-  double? peso;
-  peso = ConsoleUtils.lerDoubleTexto('Agora informe o seu peso:');
-  double? altura;
-  altura = ConsoleUtils.lerDoubleTexto('Agora informe sua altura:');
+  pessoa.peso = ConsoleUtils.lerDoubleTexto('Agora informe o seu peso:')!;
+  pessoa.altura = ConsoleUtils.lerDoubleTexto('Agora informe sua altura:')!;
 
-  double imc = peso! / altura! * altura;
+  double imc = pessoa.peso / pessoa.altura * pessoa.altura;
   print('Seu IMC é $imc!');
 }
